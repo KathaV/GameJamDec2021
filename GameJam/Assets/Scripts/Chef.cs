117d57447467ab3b4c171decb309374a22b23119
KathaVill <64733736+KathaVill@users.noreply.github.com>
2021-12-12 21:51:51 +1100

    Commented code, changed colour settings
    Colours for looking away, turning, and looking towards are now green, yellow, red, respectively

--- GameJam/Assets/Scripts/Chef.cs
+++ GameJam/Assets/Scripts/Chef.cs
@@ -14,8 +14,9 @@ public class Chef : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(turnIntermittently());
+
         sprite = GetComponent<SpriteRenderer>();
+        StartCoroutine(LookAway());
     }
 
 
@@ -26,55 +27,74 @@ public class Chef : MonoBehaviour
     }
 
 
-    IEnumerator turnIntermittently()
+    IEnumerator LookAway()
     {
-
+        isTurning = false;
+        isLooking = false;
+        // set green colour (temporary visual cue for chef state change)
+        sprite.color = new Color(0, 1, 0, 1);
         Debug.Log("Starting at" + Time.realtimeSinceStartup);
-        float offset = AvgSecondsDelay / 3;
 
+
+        // random offset to change duration slightly
+        float offset = AvgSecondsDelay / 3;
         Debug.Log(offset);
+
+        // Calculate delay to include offset, then wait
         float timeDelay = AvgSecondsDelay + Random.Range(-offset, offset);
         yield return new WaitForSeconds(timeDelay);
+
+        //Begin to turn towards player
         StartCoroutine(Turn(false));
     }
 
     IEnumerator Turn(bool lookingAway)
     {
-        sprite.color = new Color(1, 0, 0, 1);
+        isTurning = true;
+        isLooking = false;
+        // set yellow colour (temporary visual cue for chef state change)
+        sprite.color = new Color(1, 1, 0, 1);
+
+        // random offset to change duration slightly
         float offset = TurnDelay / 3;
         Debug.Log(offset);
-
         float randomOff = Random.Range(-offset, offset);
         Debug.Log("random offset " + randomOff);
+
+        // Calculate delay to include offset, then wait
         float timeDelay = TurnDelay + randomOff;
         Debug.Log("Turning at " + Time.realtimeSinceStartup + " for " + timeDelay);
-        isTurning = true;
         yield return new WaitForSeconds(timeDelay);
+
+        // Depending on if turning towards or away from player, do appropriate action
         if (!lookingAway) {
 
             StartCoroutine(Look());
         }
         else
         {
-            StartCoroutine(turnIntermittently());
+            StartCoroutine(LookAway());
         }
 
     }
 
     IEnumerator Look()
     {
-        // TODO: look code
+        // Set appropriate variables
         isLooking = true;
         isTurning = false;
 
+        // set red colour (temporary visual cue for chef state change)
+        sprite.color = new Color(1, 0, 0, 1);
+
+        // random offset to change duration slightly
         float offset = LookDelay / 3;
         float randomOff = Random.Range(-offset, offset);
         Debug.Log("random offset " + randomOff);
-        float timeDelay = LookDelay + randomOff;
 
+        // Calculate delay to include offset, then wait
+        float timeDelay = LookDelay + randomOff;
         Debug.Log("Looking at " + Time.realtimeSinceStartup + " for " + timeDelay);
-        // Change color (temporary visual cue) and wait
-        sprite.color = new Color(1, 1, 0, 1);
         yield return new WaitForSeconds(timeDelay);
 
         //Look away
