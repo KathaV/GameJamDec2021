864c7f4c325763e63e434200c24d637db6ec733d
KathaVill <64733736+KathaVill@users.noreply.github.com>
2021-12-16 16:26:40 +1100

    Working Key UI and check
    Key UI must be added to canvas from prefab, and the Text child must be assigned to KeyCollecter script in player.

--- GameJam/Assets/Scripts/LevelUp.cs
+++ GameJam/Assets/Scripts/LevelUp.cs
@@ -5,11 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class LevelUp : MonoBehaviour
 {
+    private GameObject msgController;
+    private MessageController msgScript;
+    void Start()
+    {
+
+        msgController = GameObject.FindGameObjectWithTag("MessageController");
+        msgScript = msgController.GetComponent<MessageController>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            KeyCollector collectorScript = other.gameObject.GetComponent<KeyCollector>();
+            // check if player has collected all keys
+            if (collectorScript.isFinished())
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            // else prompt player to collect all
+            else
+            {
+                msgScript.broadcast("You haven't collected all the keys! You need " + collectorScript.getRemaining() + " more to escape...");
+            }
         }
     }
 }
